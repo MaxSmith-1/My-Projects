0a5f1493c26af33297ac63139a2770a1a4228030
Max Smith <96800597+DanielSorenson@users.noreply.github.com>
2023-07-23 19:59:24 -0500

    Update Calculus.java

--- Calculus_Review/Calculus.java
+++ Calculus_Review/Calculus.java
@@ -100,27 +100,129 @@ public class Calculus {
         return answer;
     }
 
-  /*  public double arcLength(Double[] a, double b, double c){
+
+    public double areaBetweenCurves(Double[] topCurve, Double[] bottomCurve, double c, double d){
 
 
-        Double[] f = derivitiveList(a);
-        //squaring polynomial
-        ArrayList<Double> x= new ArrayList<Double>();
-        int num = f.length;
-        
-        
-        for(int i = 0; i<f.length; i++){
-            
-            for(int j = 0; j<f.length;j++){
-                
+        int l1 = topCurve.length;
+        int l2 = bottomCurve.length;
+
+        int dif = l1-l2;
+
+
+        if(dif>0){
+
+            Double[] newList = new Double[topCurve.length];
+            for(int i = 0;i< dif;i++){
+                newList[i] = topCurve[i];
             }
-            
+            for(int i = 0;i< bottomCurve.length;i++){
+                newList[i + dif] = topCurve[i + dif] - bottomCurve[i];
+            }
+            return -1 * integral(newList, c, d);
         }
+        else if(dif<0){
+
+            dif*=-1;
+            Double[] newList = new Double[bottomCurve.length];
+            for(int i = 0;i< dif;i++){
+                newList[i] = bottomCurve[i];
+            }
+            for(int i = 0;i< topCurve.length;i++){
+                newList[i + dif] = bottomCurve[i + dif] - topCurve[i];
+            }
+            return -1 * integral(newList, c, d);
+        }
+        else{
+
+            Double[] newList = new Double[topCurve.length];
+
+            for(int i = 0;i< newList.length;i++){
+                newList[i] = topCurve[i] - bottomCurve[i];
+            }
+            return -1 * integral(newList, c, d);
+        }
+
 
 
     }
 
-*/
+
+    public double lRienmannSum(Double[] function, double start, double end, int intervals){
+
+
+        Double[] range = new Double[intervals];
+        double slope = (end-start) / intervals;
+
+        for(int i = 0; i<range.length;i++){
+
+            range[i] = start + i*slope;
+        }
+
+        double answer = 0;
+
+        for(int i = 0; i<range.length;i++){
+
+            for(int j = 0; j<function.length;j++){
+
+                answer += function[j] * Math.pow(range[i], function.length - j - 1);
+            }
+        }
+
+        return answer * slope;
+
+    }
+
+
+    public double rRienmannSum(Double[] function, double start, double end, int intervals){
+
+
+        Double[] range = new Double[intervals];
+        double slope = (end-start) / intervals;
+
+        for(int i = 1; i<range.length+1;i++){
+
+            range[i-1] = start + i*slope;
+        }
+
+        double answer = 0;
+
+        for(int i = 0; i<range.length;i++){
+
+            for(int j = 0; j<function.length;j++){
+
+                answer += function[j] * Math.pow(range[i], function.length - j - 1);
+            }
+        }
+
+        return answer * slope;
+
+    }
+
+    public double mRienmanSum(Double[] function, double start, double end, int intervals){
+        return 0.0;
+    }
+    public double arcLength(Double[] a, double b, double c){
+
+
+        Double[] f = derivitiveList(a);
+
+        //squaring polynomial list into new list
+
+        // add 1 to last term of new list
+
+        //square root the new list
+
+        // integral(newList, b,c)
+
+
+        return 0.0;
+
+    }
+
+
+
+
 
 
 }
